321d730aa27e9317a6d854181d5225dbcd7c0b29
samed2023 <abdisamed2023@hotmail.com>
2021-02-25 20:39:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+months = (90-int(age))*12 
+weeks = (90-int(age))*52
+days = (90-int(age))*365
+print(f"You have {days} days, {weeks}, and {months} left.")
+#You have 12410 days, 1768 weeks, and 408 months left.
+
+
+
+
+
+
+
